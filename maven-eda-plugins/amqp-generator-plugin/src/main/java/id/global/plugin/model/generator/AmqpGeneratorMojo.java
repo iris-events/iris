d5ee62cd21f221cfa0e2303809b8ff78faa03bc2
GasperV <gasper.vrhovsek@global.id>
2021-07-16 09:18:53 +0200

    INFRA-627 - add changes from vojko to amqp generator plugin

--- maven-eda-plugins/amqp-generator-plugin/src/main/java/id/global/plugin/model/generator/AmqpGeneratorMojo.java
+++ maven-eda-plugins/amqp-generator-plugin/src/main/java/id/global/plugin/model/generator/AmqpGeneratorMojo.java
@@ -1,12 +1,8 @@
 package id.global.plugin.model.generator;
 
 import com.sun.codemodel.JCodeModel;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -18,13 +14,13 @@ import org.json.simple.parser.ParseException;
 import org.jsonschema2pojo.*;
 import org.jsonschema2pojo.rules.RuleFactory;
 
-import java.io.*;
-
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-
-import java.nio.file.Path;
+import java.nio.file.*;
+import java.util.Comparator;
+import java.util.stream.Stream;
 
 @Mojo(name = "generate-amqp-models", defaultPhase = LifecyclePhase.COMPILE)
 public class AmqpGeneratorMojo extends AbstractMojo {
@@ -53,153 +49,148 @@ public class AmqpGeneratorMojo extends AbstractMojo {
     @Parameter(property = "modelName", required = true)
     String modelName;
 
-
-    private static final String srcJava = "aaa/src/main/java";
-    private static final String schemaTempDir = "aaa/schemas";
-    private static final String tmpDirectory = "/";
-
-
-    private static final String temporaryFolder = "/models/";
-    private static final String temporarySourceFolder = temporaryFolder + "src/main/java/";
-    private static final String temporarySchemaFolder = temporaryFolder + "schemas/";
+    private  final Path tmpFolder = Paths.get("models");
+    private  final Path tmpSourceFolder = tmpFolder.resolve(Paths.get("src","main","java"));
+    private  final Path tmpSchemaFolder = tmpFolder.resolve(Paths.get("schemas"));
 
 
-    public void execute() throws MojoExecutionException, MojoFailureException {
+    public void execute() throws MojoExecutionException {
+
+
         cleanUpDirectories();
         if (artifactSource == ArtifactSource.FILE) {
             generateFromFile(fileDestination);
-        } else {
+        } else if (artifactSource == ArtifactSource.APICURIO) {
             generateFromApicurio();
+        } else {
+            throw new MojoExecutionException("Execute failed! Artifact source location not known!");
         }
     }
 
     public void generateFromFile(String fileDestination) {
 
+        String ymlContent;
         try {
-            String ymlContent = null;
-            try {
-                ymlContent = readSchemaContent(project.getBasedir() + "/target/generated/" + fileDestination);
-                parseAsyncApiJson(ymlContent, "");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            getLog().info("Generation completed!");
-
+            Paths.get(project.getBasedir().toURI()).resolve("target").resolve("generated").resolve(fileDestination);
+            ymlContent = readSchemaContent(project.getBasedir() + "/target/generated/" + fileDestination);
+            parseAsyncApiJson(ymlContent, "");
         } catch (Exception e) {
-            e.printStackTrace();
+            getLog().error("Parsing AsyncApi definition!", e);
         }
+        getLog().info("Generation completed!");
     }
 
     public String readSchemaContent(String fileName) {
-        String text = "";
         try {
-            InputStream inputStream = getFileInputStream(fileName);
-            text = IOUtils.toString(inputStream, StandardCharsets.UTF_8.name());
-            inputStream.close();
-        } catch (URISyntaxException | IOException | ParseException e) {
-            e.printStackTrace();
+            return Files.readString(Paths.get(fileName));
+        } catch (IOException e) {
+            getLog().error("Schema content read failed!", e);
         }
-        return text;
+        return null;
     }
-
-
-    public InputStream getFileInputStream(String fileName) throws URISyntaxException, IOException, ParseException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        InputStream inputStream = new FileInputStream(fileName);
-
-        if (inputStream == null) {
-            throw new IllegalArgumentException("file not found! " + fileName);
-        } else {
-            return inputStream;
-        }
-    }
-
 
     public void cleanUpDirectories() {
         try {
-            FileUtils.deleteDirectory(Path.of(project.getBasedir() + "/aaa/src").toFile());
+            deleteDirectoryAll(Path.of(project.getBasedir().toURI()).resolve(tmpFolder));
         } catch (IOException e) {
-            e.printStackTrace();
+            getLog().error("Directory cleanup failed!", e);
+
         }
-        try {
-            FileUtils.deleteDirectory(Path.of(project.getBasedir() + "/aaa/schemas").toFile());
-        } catch (IOException e) {
-            e.printStackTrace();
+    }
+
+    public static void deleteDirectoryAll(Path dir) throws IOException {
+        if (Files.exists(dir)) {
+            try (Stream<Path> walk = Files.walk(dir)) {
+                walk
+                        .sorted(Comparator.reverseOrder())
+                        .forEach(AmqpGeneratorMojo::deleteDirectory);
+            }
         }
 
     }
 
-    public String readFileContent(String fileName) {
-        String text = "";
+    public static void deleteDirectory(Path path) {
         try {
-            InputStream inputStream = getResourceFileInputStream(fileName);
-            text = IOUtils.toString(inputStream, StandardCharsets.UTF_8.name());
-            inputStream.close();
-        } catch (URISyntaxException | IOException | ParseException e) {
-            e.printStackTrace();
+            Files.delete(path);
+        } catch (IOException e) {
+            System.err.printf("Unable to delete this path : %s%n%s", path, e);
         }
-        return text;
     }
 
-    public InputStream getResourceFileInputStream(String fileName) throws URISyntaxException, IOException, ParseException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        InputStream inputStream = classLoader.getResourceAsStream(fileName);
 
-        if (inputStream == null) {
-            throw new IllegalArgumentException("file not found! " + fileName);
-        } else {
-            return inputStream;
+    public String readResourceFileContent(String fileName) {
+        String text;
+        try {
+            try (InputStream is = getClass().getClassLoader().getResourceAsStream(fileName)) {
+                assert is != null;
+                //TODO: check this out...
+                text = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+            }
+            return text;
+        } catch (IOException e) {
+            getLog().error("Cannot read resource file content!", e);
         }
+        return null;
+
     }
 
     public String readContentFromWeb(String contentUrl) throws IOException {
-        getLog().info("Reading AsyncApi definition from url: " + contentUrl);
-
-        URL url = new URL(contentUrl);
-        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-        StringBuilder builder = new StringBuilder();
-        String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            builder.append(inputLine);
-        }
-        in.close();
-        return builder.toString();
+        //TODO: for later use if needed
+        return "";
+//        getLog().info("Reading AsyncApi definition from url: " + contentUrl);
+//
+//        URL url = new URL(contentUrl);
+//        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+//        StringBuilder builder = new StringBuilder();
+//        String inputLine;
+//        while ((inputLine = in.readLine()) != null) {
+//            builder.append(inputLine);
+//        }
+//        in.close();
+//        return builder.toString();
     }
 
-    public void writeFile(String fileName, String content) {
-        FileWriter myWriter = null;
+    public void writeSchemaFile(String fileName, String content) {
+
+        Path path = Paths.get(project.getBasedir().toURI())
+                .resolve(tmpSchemaFolder)
+                .resolve(fileName);
+
+        content = content.replace("#", "");
+
+        Path p = Paths.get(project.getBasedir().toURI()).resolve(tmpSchemaFolder);
+
+
+        content = content.replace(
+                "\\/components\\/schemas\\/",
+                p.toUri().toString()
+        );
         try {
-            myWriter = new FileWriter(project.getBasedir() + temporarySchemaFolder + fileName);
-            content = content.replace("#", "file:");
-            content = content.replace(
-                    "\\/components\\/schemas\\/",
-                    project.getBasedir() + temporarySchemaFolder);
-            myWriter.write(content);
-            myWriter.close();
+            Files.writeString(path, content);
         } catch (IOException e) {
-            e.printStackTrace();
+            getLog().error("Failed to write schema file", e);
         }
 
     }
 
     public void writeFile(String fileName, String content, String path) {
-        FileWriter myWriter = null;
-        try {
-            myWriter = new FileWriter(path + "/" + fileName);
-            myWriter.write(content);
-            myWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
+        try {
+            Path clientPath = Paths.get(path);
+            Files.createDirectories(clientPath);
+
+            Path filePath = Paths.get(path, fileName);
+            Files.writeString(filePath, content);
+        } catch (IOException e) {
+            getLog().error("Failed to write file", e);
+        }
     }
 
     public void drillDown(JSONObject root, String padding) {
         getLog().info("Creating JsonSchema files for model generator!");
 
         for (String key : (Iterable<String>) root.keySet()) {
-            writeFile(key, root.get(key).toString());
+            writeSchemaFile(key, root.get(key).toString());
         }
     }
 
@@ -215,8 +206,10 @@ public class AmqpGeneratorMojo extends AbstractMojo {
 
 
         //create directory structure
-        Path.of(project.getBasedir() + temporarySourceFolder).toFile().mkdirs();
-        Path.of(project.getBasedir() + temporarySchemaFolder).toFile().mkdirs();
+        Path pathSource = Paths.get(project.getBasedir().toURI()).resolve(tmpSourceFolder);
+        Path pathSchema = Paths.get(project.getBasedir().toURI()).resolve(tmpSchemaFolder);
+        Files.createDirectories(pathSource);
+        Files.createDirectories(pathSchema);
 
 
         drillDown(schemas, " ");
@@ -234,19 +227,24 @@ public class AmqpGeneratorMojo extends AbstractMojo {
 
 
     public void generateAdditonalFiles() {
+        String stringPath = packageName.replace(".",File.separator);
 
-        String supportDataPath = project.getBasedir() + temporarySourceFolder + packageName.replace(".", "/") + "/client/";
-        Path.of(supportDataPath).toFile().mkdirs();
-        writeFile("Exchanges.java", prepareExchangeTemplate("Exchanges.java"), supportDataPath);
+        Path path = Paths.get(project.getBasedir().toURI())
+                .resolve(tmpSourceFolder)
+                .resolve(stringPath)
+                .resolve("client");
 
+        writeFile("Exchanges.java", prepareExchangeTemplate("Exchanges.java"), path.toString());
 
-        String pomFileLocation = project.getBasedir() + temporaryFolder;
-        writeFile("pom.xml", preparePomTemplate("pom.xml"), pomFileLocation);
+        Path pomPath = Paths.get(project.getBasedir().toURI())
+                .resolve(tmpFolder);
+
+        writeFile("pom.xml", preparePomTemplate("pom.xml"), pomPath.toString());
 
     }
 
-    public String prepareExchangeTemplate(String templateFile){
-        String template = readFileContent(templateFile);
+    public String prepareExchangeTemplate(String templateFile) {
+        String template = readResourceFileContent(templateFile);
         template = template.replace("!!!", modelName);
         //TODO: generate whatever will be needed here...
         template = template.replace("#####", """
@@ -258,8 +256,8 @@ public class AmqpGeneratorMojo extends AbstractMojo {
         return template;
     }
 
-    public String preparePomTemplate(String templateFile){
-        String pomtemplate = readFileContent(templateFile);
+    public String preparePomTemplate(String templateFile) {
+        String pomtemplate = readResourceFileContent(templateFile);
         pomtemplate = pomtemplate.replace("XXXX", modelName);
         pomtemplate = pomtemplate.replace("YYYY", modelVersion);
         return pomtemplate;
@@ -274,12 +272,12 @@ public class AmqpGeneratorMojo extends AbstractMojo {
         }
     }
 
-    public void generate(String fileName) throws IOException, ParseException {
+    public void generate(String fileName) throws IOException {
 
         JCodeModel codeModel = new JCodeModel();
-        URL source = Path.of(project.getBasedir() + temporarySchemaFolder + fileName).toFile().toURI().toURL();
-        Path.of(project.getBasedir() + temporarySourceFolder).toFile().mkdirs();
 
+        Path clientPath = Paths.get(project.getBasedir().toURI()).resolve(tmpSourceFolder);
+        Files.createDirectories(clientPath);
 
         SchemaMapper mapper = new SchemaMapper(
                 new RuleFactory(
@@ -289,14 +287,13 @@ public class AmqpGeneratorMojo extends AbstractMojo {
                 new SchemaGenerator());
 
 
+
+        Path schemes = Paths.get(project.getBasedir().toURI()).resolve(tmpSchemaFolder).resolve(fileName);
         //TODO: investigate what ClassName does!
-        mapper.generate(codeModel, "ClassName", packageName, source);
+        mapper.generate(codeModel, "ClassName", packageName, schemes.toUri().toURL());
 
-
-        codeModel.build(Path.of(project.getBasedir() + temporarySourceFolder).toFile());
+        codeModel.build(clientPath.toFile());
     }
-
-
 
 
     public void generateFromApicurio() {
@@ -316,12 +313,12 @@ public class AmqpGeneratorMojo extends AbstractMojo {
                     String applicationName = o.toString().split(":")[0]; //TODO: make it nicer
                     parseAsyncApiJson(ymlContent, applicationName);
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    getLog().error("Generating from appicurio failed!", e);
                 }
             }
 
         } catch (IOException | ParseException e) {
-            e.printStackTrace();
+            getLog().error("Reading from appicurio failed!", e);
         }
     }
 }
