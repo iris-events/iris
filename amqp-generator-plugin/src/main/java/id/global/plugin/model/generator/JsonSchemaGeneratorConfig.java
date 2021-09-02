a22b0dc9308c72dc54d4ba8250762b38b90fd2b5
vojkor <vojko@global.id>
2021-09-02 15:01:10 +0200

    INFRA-838 remove commented code

--- amqp-generator-plugin/src/main/java/id/global/plugin/model/generator/JsonSchemaGeneratorConfig.java
+++ amqp-generator-plugin/src/main/java/id/global/plugin/model/generator/JsonSchemaGeneratorConfig.java
@@ -33,11 +33,6 @@ public class JsonSchemaGeneratorConfig {
         @Override
         public boolean isUsePrimitives(){return true;};
 
-//        @Override
-//        public boolean isUseDoubleNumbers() {
-//            return true;
-//        }
-
         @Override
         public String getDateTimeType() {
             return "java.time.ZonedDateTime";
