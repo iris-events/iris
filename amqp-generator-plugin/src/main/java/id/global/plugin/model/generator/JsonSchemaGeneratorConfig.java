bd552ec222270373f53f504461617be13c61a6f7
vojkor <vojko@global.id>
2021-09-02 13:46:00 +0200

    INFRA-838 use BigDecimal, use ZonedDateTime

--- amqp-generator-plugin/src/main/java/id/global/plugin/model/generator/JsonSchemaGeneratorConfig.java
+++ amqp-generator-plugin/src/main/java/id/global/plugin/model/generator/JsonSchemaGeneratorConfig.java
@@ -33,19 +33,24 @@ public class JsonSchemaGeneratorConfig {
         @Override
         public boolean isUsePrimitives(){return true;};
 
-        @Override
-        public boolean isUseDoubleNumbers() {
-            return true;
-        }
+//        @Override
+//        public boolean isUseDoubleNumbers() {
+//            return true;
+//        }
 
         @Override
         public String getDateTimeType() {
-            return "java.time.LocalDateTime";
+            return "java.time.ZonedDateTime";
         }
 
         @Override
         public String getDateType() {
             return "java.time.LocalDate";
+        }
+
+        @Override
+        public boolean isUseBigDecimals() {
+            return true;
         }
 
     };
