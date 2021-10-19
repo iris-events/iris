f96a8cea38deebd3e75ddf11d29639dee5c8cd14
GasperV <gasper.vrhovsek@global.id>
2021-10-19 11:06:45 +0200

    INFRA-972: add name property to both config classes

--- runtime/src/main/java/id/global/event/messaging/runtime/configuration/AmqpBuildConfiguration.java
+++ runtime/src/main/java/id/global/event/messaging/runtime/configuration/AmqpBuildConfiguration.java
@@ -4,7 +4,7 @@ import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
 
-@ConfigRoot(phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
+@ConfigRoot(name = "amqp", phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
 public final class AmqpBuildConfiguration {
     /**
      * disable initialization of consumers
