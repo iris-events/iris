e1c531e3888530b75e04d42e7bc97e7010acd70f
Nejc Tomažič <nejc.tomazic@global.id>
2022-01-04 13:51:43 +0100

    INFRA-1135: add all known roles

--- src/main/java/id/global/common/auth/jwt/Role.java
+++ src/main/java/id/global/common/auth/jwt/Role.java
@@ -9,7 +9,22 @@ public enum Role {
     GROUP_OWNER("GROUP_OWNER"),
     ADMIN_WALLET("admin.wallet"),
     ADMIN_REWARD("admin.reward"),
-    ADMIN_MERCHANT("admin.merchant");
+    ADMIN_MERCHANT("admin.merchant"),
+    ADMIN_GROUP_CAPABILITIES_GROUPS_OF_GROUPS("admin.group-capabilities.groups-of-groups"),
+    ADMIN_GROUP_CARDS("admin.group_cards"),
+    ADMIN_IDENTITY_RESTORE("admin.identity-restore"),
+    ADMIN_IDENTITY_RESTORE_SUPERVISOR("admin.identity-restore-supervisor"),
+    ADMIN_ATTESTATION_AGENCY("admin.attestation-agency"),
+    ADMIN_WHITELISTED_PHONE_NUMBERS("admin.whitelisted-phone-numbers"),
+    ADMIN_RESERVED_NAMES("admin.reserved-names"),
+    ADMIN_RESERVED_NAMES_CLAIM("admin.reserved-names.claim"),
+    ADMIN_NOTIFICATION_TEMPLATE_SETS("admin.notification-template-sets"),
+    ADMIN_NOTIFICATIONS("admin.notifications"),
+    ADMIN_ATTESTATION_APP_MANAGER("admin.attestation-app-manager"),
+    ADMIN_TOS("admin.tos"),
+    ADMIN_WHITELISTED_GROUP_LIMIT("admin.whitelisted-group-limit"),
+    ADMIN_WHITELISTED_RECOVERY_IDENTITY("admin.whitelisted-recovery-identity"),
+    ADMIN_HARDWIRED_BADGES("admin.hardwired-badges");
 
     private final String value;
 
