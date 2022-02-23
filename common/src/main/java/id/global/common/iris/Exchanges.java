7ee287db8e08f97f838e31e034e64883892eef3a
GasperV <gasper.vrhovsek@global.id>
2022-02-23 13:39:36 +0100

    INFRA-1331: fix ROADCAST typo to BROADCAST

--- common/src/main/java/id/global/common/iris/Exchanges.java
+++ common/src/main/java/id/global/common/iris/Exchanges.java
@@ -6,7 +6,7 @@ public enum Exchanges {
     ERROR("error"),
     FRONTEND("frontend"),
     RETRY("retry"),
-    ROADCAST("broadcast"),
+    BROADCAST("broadcast"),
     SESSION("session"),
     USER("user");
 
