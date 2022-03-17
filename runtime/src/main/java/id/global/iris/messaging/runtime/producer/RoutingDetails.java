f3a3079c7781eb0a3ced65072df0eb9ace1096d5
Nejc Tomažič <nejc.tomazic@global.id>
2022-03-17 12:22:14 +0100

    INFRA-346: update client message routing

--- runtime/src/main/java/id/global/iris/messaging/runtime/producer/RoutingDetails.java
+++ runtime/src/main/java/id/global/iris/messaging/runtime/producer/RoutingDetails.java
@@ -3,5 +3,10 @@ package id.global.iris.messaging.runtime.producer;
 import id.global.common.annotations.amqp.ExchangeType;
 import id.global.common.annotations.amqp.Scope;
 
-public record RoutingDetails(String exchange, ExchangeType exchangeType, String routingKey, Scope scope, String userId) {
+public record RoutingDetails(String eventName,
+                             String exchange,
+                             ExchangeType exchangeType,
+                             String routingKey,
+                             Scope scope,
+                             String userId) {
 }
