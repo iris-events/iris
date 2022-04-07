6140e22953d5de44cfb2753b9d3ad28dd2e5df2f
Nejc Tomažič <nejc.tomazic@global.id>
2022-04-07 11:38:24 +0200

    INFRA-346: initialize producer connection on startup

--- runtime/src/main/java/id/global/iris/messaging/runtime/connection/ProducerConnectionProvider.java
+++ runtime/src/main/java/id/global/iris/messaging/runtime/connection/ProducerConnectionProvider.java
@@ -1,11 +1,13 @@
 package id.global.iris.messaging.runtime.connection;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
 import id.global.iris.messaging.runtime.InstanceInfoProvider;
 import id.global.iris.messaging.runtime.configuration.AmqpConfiguration;
 import id.global.iris.messaging.runtime.health.IrisHealthCheck;
+import io.quarkus.runtime.StartupEvent;
 
 @ApplicationScoped
 public class ProducerConnectionProvider extends AbstractConnectionProvider {
@@ -15,6 +17,13 @@ public class ProducerConnectionProvider extends AbstractConnectionProvider {
     public ProducerConnectionProvider(ConnectionFactoryProvider connectionFactoryProvider,
             InstanceInfoProvider instanceInfoProvider, AmqpConfiguration configuration, IrisHealthCheck healthCheck) {
         super(connectionFactoryProvider, instanceInfoProvider, configuration, healthCheck);
+    }
+
+    /**
+     * When service is only producing events, it needs to obtain connection in order to appear ready for Health check.
+     */
+    public void onApplicationStart(@Observes StartupEvent event) {
+        getConnection();
     }
 
     @Override
