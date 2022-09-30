c31b8ce85e6fc02ff1c99ca163ac0e2d8d876ef3
Nejc Tomažič <nejc.tomazic@global.id>
2022-09-30 11:06:07 +0200

    INFRA-1766: fix unremovable bean scan

--- extension/deployment/src/main/java/id/global/iris/messaging/deployment/EventMessagingProcessor.java
+++ extension/deployment/src/main/java/id/global/iris/messaging/deployment/EventMessagingProcessor.java
@@ -123,7 +123,7 @@ class EventMessagingProcessor {
     @BuildStep
     UnremovableBeanBuildItem unremovable() {
         // Any bean that contains or is annotated with annotation defined within the given package is considered unremovable
-        return UnremovableBeanBuildItem.beanClassAnnotation("id.global.common.iris.annotations");
+        return UnremovableBeanBuildItem.beanClassAnnotation("id.global.iris.common.annotations");
     }
 
     @SuppressWarnings("unused")
