31cce831b3630fddfe48444cefe13e78edc6e34b
Gasper Vrhovsek <gasper.vrhovsek@gmail.com>
2022-12-08 10:33:09 +0100

    INFRA-1816: scan Message and build info item

--- extension/deployment/src/main/java/id/global/iris/messaging/deployment/builditem/MessageInfoBuildItem.java
+++ extension/deployment/src/main/java/id/global/iris/messaging/deployment/builditem/MessageInfoBuildItem.java
@@ -1,18 +1,66 @@
 package id.global.iris.messaging.deployment.builditem;
 
+import id.global.iris.common.annotations.ExchangeType;
+import id.global.iris.common.annotations.Scope;
 import io.quarkus.builder.item.MultiBuildItem;
 import org.jboss.jandex.DotName;
+import org.jboss.jandex.Type;
 
 public class MessageInfoBuildItem extends MultiBuildItem {
     // this might be better as Type, it's the same in MessageHandlerInfoBuildItem
-    private final DotName annotatedClass;
+    private final Type annotatedClassType;
+    private final ExchangeType exchangeType;
+    private final String name;
+    private final String routingKey;
+    private final Scope scope;
+    private final int ttl;
+    private final String deadLetter;
+    private final boolean persistent;
+
+    public MessageInfoBuildItem(final Type annotatedClassType, final ExchangeType exchangeType, final String name,
+            final String routingKey,
+            final Scope scope, final int ttl, final String deadLetter, final boolean persistent) {
+        this.annotatedClassType = annotatedClassType;
+        this.exchangeType = exchangeType;
+        this.name = name;
+        this.routingKey = routingKey;
+        this.scope = scope;
+        this.ttl = ttl;
+        this.deadLetter = deadLetter;
+        this.persistent = persistent;
+    }
+
+    public Type getAnnotatedClassType() {
+        return annotatedClassType;
+    }
+
+    public ExchangeType getExchangeType() {
+        return exchangeType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRoutingKey() {
+        return routingKey;
+    }
+
+    public Scope getScope() {
+        return scope;
+    }
+
+    public int getTtl() {
+        return ttl;
+    }
+
+    public String getDeadLetter() {
+        return deadLetter;
+    }
+
+    public boolean isPersistent() {
+        return persistent;
+    }
+
     // we'll also need additional fields for declaring exchange in case of a produced event
-
-    public MessageInfoBuildItem(DotName annotatedClass) {
-        this.annotatedClass = annotatedClass;
-    }
-
-    public DotName getAnnotatedClass() {
-        return annotatedClass;
-    }
 }
