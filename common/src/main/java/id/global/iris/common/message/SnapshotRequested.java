bdb024e0caf6d9a470dda43aa94c41de640396d1
Nejc Tomažič <nejc.tomazic@global.id>
2023-02-16 14:12:53 +0100

    update common message serialization

--- common/src/main/java/id/global/iris/common/message/SnapshotRequested.java
+++ common/src/main/java/id/global/iris/common/message/SnapshotRequested.java
@@ -1,10 +1,13 @@
 package id.global.iris.common.message;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import id.global.iris.common.annotations.ExchangeType;
 import id.global.iris.common.annotations.IrisGenerated;
 import id.global.iris.common.annotations.Message;
 
 @IrisGenerated
 @Message(name = "snapshot-requested", exchangeType = ExchangeType.TOPIC)
-public record SnapshotRequested(String resourceType, String resourceId) {
+public record SnapshotRequested(@JsonProperty("resource_type") String resourceType,
+                                @JsonProperty("resource_id") String resourceId) {
 }
