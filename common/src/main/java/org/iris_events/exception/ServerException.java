abe639f8fc584d829a0acdaf99be2483a5cbd66e
Gasper Vrhovsek <gasper.vrhovsek@gmail.com>
2023-10-06 09:18:50 +0200

    Add retry flag to server exception, handle in IrisExceptionManager

--- common/src/main/java/org/iris_events/exception/ServerException.java
+++ common/src/main/java/org/iris_events/exception/ServerException.java
@@ -6,17 +6,34 @@ public class ServerException extends MessagingException {
     private static final ErrorType ERROR_TYPE = ErrorType.INTERNAL_SERVER_ERROR;
     private final boolean shouldNotifyFrontend;
 
-    public ServerException(final String clientCode, final String message, boolean notifyFrontend) {
+    private final boolean shouldRetry;
+
+    public ServerException(final String clientCode, final String message, boolean notifyFrontend, boolean retry) {
         super(ERROR_TYPE, clientCode, message);
         this.shouldNotifyFrontend = notifyFrontend;
+        this.shouldRetry = retry;
+    }
+
+    public ServerException(final String clientCode, final String message, boolean notifyFrontend, boolean retry,
+            final Throwable cause) {
+        super(ERROR_TYPE, clientCode, message, cause);
+        this.shouldNotifyFrontend = notifyFrontend;
+        this.shouldRetry = retry;
+    }
+
+    public ServerException(final String clientCode, final String message, boolean notifyFrontend) {
+        this(clientCode, message, notifyFrontend, true);
     }
 
     public ServerException(final String clientCode, final String message, boolean notifyFrontend, final Throwable cause) {
-        super(ERROR_TYPE, clientCode, message, cause);
-        this.shouldNotifyFrontend = notifyFrontend;
+        this(clientCode, message, notifyFrontend, true, cause);
     }
 
     public boolean shouldNotifyFrontend() {
         return shouldNotifyFrontend;
+    }
+
+    public boolean shouldRetry() {
+        return shouldRetry;
     }
 }
