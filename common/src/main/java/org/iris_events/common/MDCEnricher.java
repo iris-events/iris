6d41b7536db4a3799d4bbf05a2d8ccbd8da36a0b
Nejc Tomažič <nejc.tomazic@global.id>
2024-08-06 16:27:24 +0200

    fill correlation id MDC property from basic properties native field rather than the header

--- common/src/main/java/org/iris_events/common/MDCEnricher.java
+++ common/src/main/java/org/iris_events/common/MDCEnricher.java
@@ -2,7 +2,6 @@ package org.iris_events.common;
 
 import static org.iris_events.common.MessagingHeaders.Message.CLIENT_TRACE_ID;
 import static org.iris_events.common.MessagingHeaders.Message.CLIENT_VERSION;
-import static org.iris_events.common.MessagingHeaders.Message.CORRELATION_ID;
 import static org.iris_events.common.MessagingHeaders.Message.EVENT_TYPE;
 import static org.iris_events.common.MessagingHeaders.Message.SESSION_ID;
 import static org.iris_events.common.MessagingHeaders.Message.USER_ID;
@@ -20,7 +19,7 @@ public class MDCEnricher {
         getStringHeader(properties, SESSION_ID).ifPresent(s -> MDC.put(MDCProperties.SESSION_ID, s));
         getStringHeader(properties, USER_ID).ifPresent(s -> MDC.put(MDCProperties.USER_ID, s));
         getStringHeader(properties, CLIENT_TRACE_ID).ifPresent(s -> MDC.put(MDCProperties.CLIENT_TRACE_ID, s));
-        getStringHeader(properties, CORRELATION_ID).ifPresent(s -> MDC.put(MDCProperties.CORRELATION_ID, s));
+        Optional.ofNullable(properties.getCorrelationId()).ifPresent(s -> MDC.put(MDCProperties.CORRELATION_ID, s));
         getStringHeader(properties, EVENT_TYPE).ifPresent(s -> MDC.put(MDCProperties.EVENT_TYPE, s));
         getStringHeader(properties, CLIENT_VERSION).ifPresent(s -> MDC.put(MDCProperties.CLIENT_VERSION, s));
     }
